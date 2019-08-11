3fca378630256075e15146f186c10be7287133d4
league <league@imac-7.attlocal.net>
2019-08-11 11:28:57 -0700

    Finished sorting, worked on searchTests

--- src/_01_Searching_Algorithms/_04_SearchTest.java
+++ src/_01_Searching_Algorithms/_04_SearchTest.java
@@ -7,10 +7,21 @@ import org.junit.jupiter.api.Test;
 class _04_SearchTest {
 
 	// A MINIMUM OF 3 TESTS ARE REQUIRED FOR EACH METHOD
+	String[] unsorted = new String[] {"hi", "dancer", "dictionary", "lettuce", "show", "wheel", "cantrip", "unicorn", "minion", "tree"};
+	int[] unsortedInt = new int[100];
+	int[] sortedInt = new int[100];
+	int[] evenlySpacedInt = new int[100];
+	_00_LinearSearch linear = new _00_LinearSearch();
+	_01_BinarySearch binary = new _01_BinarySearch();
+	_02_InterpolationSearch interpolation = new _02_InterpolationSearch();
+	_03_ExponentialSearch exponential = new _03_ExponentialSearch();
 	
 	@Test
 	public void testLinearSearch() {
 		//1. use the assertEquals method to test your linear search method.
+		assertEquals(linear.linearSearch(unsorted, "dancer"),1);
+		assertEquals(linear.linearSearch(unsorted, "hi"),0);
+		assertEquals(linear.linearSearch(unsorted, "lettuce"),3);
 	}
 
 	@Test
